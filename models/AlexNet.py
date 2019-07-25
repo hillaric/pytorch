7450902f3ffc5653d737e2b88d77ec7362ded49c
hillaric <50513602+hillaric@users.noreply.github.com>
2019-07-25 10:27:18 +0800

    Update AlexNet.py

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -1,1 +1,50 @@
-
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=2):
+        super(AlexNet,self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv2 = nn.Conv2d(64, 192, kernel_size=5, padding=2)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(192, 386, kernel_size=3, padding=1)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.conv4 = nn.Conv2d(386, 256, kernel_size=3, padding=1)
+        self.relu4 = nn.ReLU(inplace=True)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.relu5 = nn.ReLU(inplace=True)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2)
+        
+        self.dropout1 = nn.Dropout()
+        self.fc1 = nn.Linear(256 * 6 * 6, 4096)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout2 = nn.Dropout()
+        self.fc2 = nn.Linear(4096, 4096)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc3 = nn.Linear(4096, num_classes)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+        
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+        
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.conv4(x)
+        x = self.relu4(x)
+        x = self.conv5(x)
+        x = self.relu5(x)
+        x = self.maxpool3(x)
+        
+        x = x.view(-1, 256 * 6 * 6)
+        x = self.dropout1(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        return x
